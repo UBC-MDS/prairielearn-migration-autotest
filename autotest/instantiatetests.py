4e3511b63c1ed36d4381ca40b0d345cf43c84ac7
Vincent <vliu1@ualberta.ca>
2024-07-05 12:32:17 -0700

    add DISPATCH

--- autotest/instantiatetests.py
+++ autotest/instantiatetests.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 parser = argparse.ArgumentParser()
 parser.add_argument("--pl_question_folder", default="pl-ubc-dsci523/questions")
 parser.add_argument("--config_path", default="autotests.yml")
+parser.add_argument("--timeout", default=30, type=int)
 args = parser.parse_args()
 
 
@@ -22,6 +23,20 @@ def find_folders_with_file(base_dir, target_file="question.html"):
             folders_with_file.append(root)
 
     return folders_with_file
+
+
+def add_quotation(test_string, add_f=False):
+    if add_f:
+        return "f'" + test_string + "'"
+    else:
+        return "'" + test_string + "'"
+
+
+def add_quotation_auto(test_string):
+    if "{" in test_string and "}" in test_string:
+        return add_quotation(test_string, add_f=True)
+    else:
+        return add_quotation(test_string, add_f=False)
 
 
 logging.info("loading template autotests.yml...")
@@ -66,7 +81,7 @@ for question_folder in all_question_folders:
         "enabled": True,
         "image": autotest_config["pl"]["image"],
         "entrypoint": autotest_config["pl"]["entrypoint"],
-        "timeout": 30,
+        "timeout": args.timeout,
     }
     if autotest_config["pl"]["server_files"] != "":
         question_info["externalGradingOptions"]["serverFilesCourse"] = [
@@ -83,14 +98,8 @@ for question_folder in all_question_folders:
     )
     test_path = "{}/{}".format(tests_folder, autotest_config["pl"]["test_file_name"])
 
-    snippets, error_handling_snippets, prefix_code, postfix_code = (
-        find_autotest_variables(
-            solution_path,
-            test_delimiter="# AUTOTEST ",
-            error_delimiter="# EXPECT-ERROR ",
-            prefix_delimiter="# SOLUTION",
-            postfix_delimiter="# TESTSETUP",
-        )
+    snippets, error_handling_snippets, dispatch_snippets, prefix_code, postfix_code = (
+        find_autotest_variables(solution_path)
     )
 
     logging.info("found snippets to test: [{}]".format(",".join(snippets)))
@@ -105,7 +114,8 @@ for question_folder in all_question_folders:
     test_file = autotest_config["testfile_template"]
     test_count = 0
 
-    for snippet in snippets:
+    for i in range(len(snippets)):
+        snippet = snippets[i]
         if code_language == "r":
             import rpy2.robjects as robjects
 
@@ -154,9 +164,7 @@ for question_folder in all_question_folders:
                 )
 
         else:
-            logging.info(
-                f"executing {solution_path} to determine type...\n############"
-            )
+            logging.info(f"executing {solution_path} to determine type...")
             # execute solution in python
             solution_env = {}
             with open(solution_path, "r", encoding="utf-8") as f:
@@ -166,21 +174,33 @@ for question_folder in all_question_folders:
             exec(code_string, solution_env)
             os.chdir(current_wd)
             dispatch_template = Template(autotest_config["dispatch"])
-            dispatch_result = eval(
-                dispatch_template.render({"snippet": snippet}), solution_env
-            )
+            if dispatch_snippets is None:
+                dispatch_result = eval(
+                    dispatch_template.render({"snippet": snippets[i]}), solution_env
+                )
+            else:
+                # use dispatch_snippets
+                dispatch_result = eval(
+                    dispatch_template.render({"snippet": dispatch_snippets[i]}),
+                    solution_env,
+                )
             dispatch_result = dispatch_result.__name__
-            logging.info("############")
 
             if dispatch_result not in autotest_config["test_expr_templates"].keys():
                 raise Exception(
                     f"Unknown data type {dispatch_result}. Need to update the check function."
                 )
 
+            # generate test files
             test_templates = autotest_config["test_expr_templates"][dispatch_result]
             for template in test_templates:
                 test_expr_template = Template(template["test"])
-                test_expr = test_expr_template.render({"snippet": snippet})
+                test_string = add_quotation_auto(
+                    "test " + test_expr_template.render({"snippet": snippet})
+                )
+                test_expr = add_quotation_auto(
+                    test_expr_template.render({"snippet": snippet})
+                )
                 # variable_type_to_check = eval(test_expr, solution_env).__name__
                 # check_list, check_tuple, check_scalar, check_numpy_array_features, check_numpy_array_sanity
 
@@ -190,6 +210,7 @@ for question_folder in all_question_folders:
                         "score": template["point"] / total_snippets,
                         "count": test_count,
                         "check_fn": template["check_fn"],
+                        "test_string": test_string,
                         "test_expr": test_expr,
                         "solution_params": "",
                         "submission_params": "prefix_code='{}', postfix_code='{}'".format(
@@ -199,15 +220,15 @@ for question_folder in all_question_folders:
                 )
                 test_count += 1
 
-        for snippet in error_handling_snippets:
-            if code_language == "r":
-                error_case_template = Template(autotest_config["error_case_template"])
-                test_file += error_case_template.render(
-                    {"score": 1 / total_snippets, "snippet": snippet}
-                )
-            else:
-                # TODO: error handling test for python
-                raise NotImplementedError
+    for snippet in error_handling_snippets:
+        if code_language == "r":
+            error_case_template = Template(autotest_config["error_case_template"])
+            test_file += error_case_template.render(
+                {"score": 1 / total_snippets, "snippet": snippet}
+            )
+        else:
+            # TODO: error handling test for python
+            raise NotImplementedError
 
     if total_snippets > 0:
         logging.info(f"added test file to {test_path}")
